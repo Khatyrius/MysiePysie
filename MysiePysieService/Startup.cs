474d09915262681298a8c8299423a69977123382
Khatyrius <bartosz_musielak@hotmail.com>
2020-05-04 16:26:07 +0200

    Added test scenerios and MVC project as a base for the rest

---
+++ MysiePysieService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MysiePysieService.Startup))]
+namespace MysiePysieService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
