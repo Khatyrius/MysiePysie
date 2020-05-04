fb8acdc4266b38b00dcd308711fb1b89bd26c87e
Khatyrius <bartosz_musielak@hotmail.com>
2020-05-05 00:18:14 +0200

    fixed package problem and generated test steps

---
+++ MysiePysieService.Tests/Steps/TeacherControlSteps.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MysiePysieService.Tests.Steps
+{
+    [Binding]
+    public class TeacherControlSteps
+    {
+        [Given(@"I'm on a tachers add page")]
+        public void GivenIMOnATachersAddPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I clicked on add tacher button")]
+        public void GivenIClickedOnAddTacherButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on tachers page")]
+        public void GivenIMOnTachersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on a tachers update page")]
+        public void GivenIMOnATachersUpdatePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have perrmision to update a tacher")]
+        public void GivenIHavePerrmisionToUpdateATacher()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on a teachers page")]
+        public void GivenIMOnATeachersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on a tachers page")]
+        public void GivenIMOnATachersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a list of tachers open")]
+        public void GivenIHaveAListOfTachersOpen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"All tacher info is filled in")]
+        public void WhenAllTacherInfoIsFilledIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on show all tachers")]
+        public void WhenIClickOnShowAllTachers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on update")]
+        public void WhenIClickOnUpdate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on find teacher by subject")]
+        public void WhenIClickOnFindTeacherBySubject()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I fill in subject name")]
+        public void WhenIFillInSubjectName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on show a single tacher")]
+        public void WhenIClickOnShowASingleTacher()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I fill in tacher id")]
+        public void WhenIFillInTacherId()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on delete teacher")]
+        public void WhenIClickOnDeleteTeacher()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The tacher should be added to the database")]
+        public void ThenTheTacherShouldBeAddedToTheDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"A list of tachers appears on page:")]
+        public void ThenAListOfTachersAppearsOnPage(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Teacher is updated")]
+        public void ThenTeacherIsUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"a teacher who teaches that subject is returned")]
+        public void ThenATeacherWhoTeachesThatSubjectIsReturned()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"A single tacher object matching the id should be returned")]
+        public void ThenASingleTacherObjectMatchingTheIdShouldBeReturned()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Teacher no longer exists in database")]
+        public void ThenTeacherNoLongerExistsInDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ MysiePysieService.Tests/Steps/UserSteps.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MysiePysieService.Tests.Steps
+{
+    [Binding]
+    public class UserSteps
+    {
+        [Given(@"I'm on register page")]
+        public void GivenIMOnRegisterPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have filled in the data")]
+        public void GivenIHaveFilledInTheData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a list of users to add")]
+        public void GivenIHaveAListOfUsersToAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on login page")]
+        public void GivenIMOnLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"user info is filled in")]
+        public void GivenUserInfoIsFilledIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on any page")]
+        public void GivenIMOnAnyPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I'm on users page")]
+        public void GivenIMOnUsersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"i have a list of users")]
+        public void GivenIHaveAListOfUsers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"admin status")]
+        public void GivenAdminStatus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press register user")]
+        public void WhenIPressRegisterUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"all data has valid form")]
+        public void WhenAllDataHasValidForm()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"i click on add users from list")]
+        public void WhenIClickOnAddUsersFromList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on login button")]
+        public void WhenIClickOnLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click logout button")]
+        public void WhenIClickLogoutButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on delete user")]
+        public void WhenIClickOnDeleteUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on update user")]
+        public void WhenIClickOnUpdateUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"New user is created")]
+        public void ThenNewUserIsCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"all users from the list are created")]
+        public void ThenAllUsersFromTheListAreCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User is logged in")]
+        public void ThenUserIsLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User gets logged out and redirected to home site")]
+        public void ThenUserGetsLoggedOutAndRedirectedToHomeSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"user gets deleted")]
+        public void ThenUserGetsDeleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User gets updated")]
+        public void ThenUserGetsUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
